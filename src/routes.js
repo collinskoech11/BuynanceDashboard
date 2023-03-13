70b10554b14d61ccfdac163c6da823c16e052308
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2023-03-14 00:16:59 +0300

    update navigation

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,8 @@ const SellOrders = React.lazy(() => import('./views/orders/SellOrders'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard},
-  { path: '/new_buy_order', name: 'Buy', element: Dashboard },
-  { path: '/new_sell_order', name: 'Sell', element: Dashboard },
+  { path: '/buy_crypto', name: 'Buy', element: Dashboard },
+  { path: '/sell_order', name: 'Sell', element: Dashboard },
   { path: '/sell_orders', name: 'Sells', element: SellOrders },
   { path: '/buy_orders', name: 'Buys', element: BuyOrders },
   { path: '/my_orders/order', name: 'myOrders', element: Dashboard },
